f2cc358187b1c58ad9bb0a216242297f8a3c6739
Sherief Gharraph <gharraph@msn.com>
2012-10-09 21:23:33 -0400

    initialized new rails prject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarFinder::Application.config.secret_token = 'd342e368b27ac1ba6c587e72cd8aa8840a936d7d341c11af50072aebfca8f3947a328686b31dfe3d97924b3bb9588090f59fdf388a0099bb3e51e6fe4065ca3c'
